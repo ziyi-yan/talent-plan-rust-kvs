8f18a4d28a95705c8c2b8a913c0a1e9fe75b174e
Ziyi Yan <ziyi.yan@foxmail.com>
2020-03-10 20:55:25 +0800

    refactor from clippy to structopt
    Signed-off-by: Ziyi Yan <ziyi.yan@foxmail.com>

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,46 +1,26 @@
-use clap::{App, Arg, SubCommand};
 use kvs::KvStore;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = env!("CARGO_PKG_NAME"), about = env!("CARGO_PKG_DESCRIPTION"), author = env!("CARGO_PKG_AUTHORS"), version = env!("CARGO_PKG_VERSION"))]
+enum Opt {
+    Set { key: String, value: String },
+    Get { key: String },
+    Rm { key: String },
+}
 
 fn main() {
-    let matches = App::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .subcommand(
-            SubCommand::with_name("set")
-                .about("set a value for key")
-                .arg(Arg::with_name("key").help("the key").required(true))
-                .arg(Arg::with_name("value").help("the value").required(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("get")
-                .about("get a value by key")
-                .arg(Arg::with_name("key").help("the key").required(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("rm")
-                .about("rm a value by key")
-                .arg(Arg::with_name("key").help("the key").required(true)),
-        )
-        .get_matches();
+    let opt = Opt::from_args();
     let mut kvs = KvStore::new();
-    match matches.subcommand() {
-        ("set", Some(sub_m)) => {
-            let key = sub_m.value_of("key").unwrap();
-            let value = sub_m.value_of("value").unwrap();
-            kvs.set(key.to_owned(), value.to_owned());
+    match opt {
+        Opt::Set { key, value } => {
+            kvs.set(key, value);
         }
-        ("get", Some(sub_m)) => {
-            let key = sub_m.value_of("key").unwrap();
-            kvs.get(key.to_owned());
+        Opt::Get { key } => {
+            kvs.get(key);
         }
-        ("rm", Some(sub_m)) => {
-            let key = sub_m.value_of("key").unwrap();
-            kvs.remove(key.to_owned());
-        }
-        _ => {
-            eprintln!("{}", matches.usage());
-            std::process::exit(1)
+        Opt::Rm { key } => {
+            kvs.remove(key);
         }
     }
 }
