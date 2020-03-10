d0e4fa17bd9da17c92c46efa0733005446ecd519
Ziyi Yan <ziyi.yan@foxmail.com>
2020-03-10 16:05:08 +0800

    make the tests build
    Signed-off-by: Ziyi Yan <ziyi.yan@foxmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+pub struct KvStore {}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        KvStore {}
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        unimplemented!()
+    }
+    pub fn get(&mut self, key: String) -> Option<String> {
+        unimplemented!()
+    }
+    pub fn remove(&mut self, key: String) {
+        unimplemented!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
