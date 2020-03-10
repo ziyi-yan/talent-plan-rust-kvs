bc35e988edbfc642d1e5715b52ff667eb02039d4
Ziyi Yan <ziyi.yan@foxmail.com>
2020-03-10 20:32:00 +0800

    add documentation comments
    Signed-off-by: Ziyi Yan <ziyi.yan@foxmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,32 @@
+//! kvs provides a key-vale store in memory.
+#![deny(missing_docs)]
 use std::collections::HashMap;
 
+/// KvStore is an in-memory key-value store.
 pub struct KvStore {
     m: HashMap<String, String>,
 }
 
 impl KvStore {
+    /// Create a key-value store.
     pub fn new() -> KvStore {
         KvStore { m: HashMap::new() }
     }
 
+    /// Set a value associated with key.
     pub fn set(&mut self, key: String, value: String) {
         self.m.insert(key, value);
     }
+
+    /// Get a value by key.
     pub fn get(&mut self, key: String) -> Option<String> {
         match self.m.get(&key) {
             Some(value) => Some(value.clone()),
             None => None,
         }
     }
+
+    /// Remove a value by key.
     pub fn remove(&mut self, key: String) {
         self.m.remove(&key);
     }
