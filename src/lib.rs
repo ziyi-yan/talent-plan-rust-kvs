b21791b9645048a61f05f0fcbf325590660c83b9
Ziyi Yan <ziyi.yan@foxmail.com>
2020-06-24 20:48:29 +0800

    project-2 part-6 cache and update index in memory

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ use std::{fs, path::Path};
 /// KvStore is an in-memory key-value store.
 pub struct KvStore {
     datafile: std::path::PathBuf,
-    w: io::BufWriter<fs::File>,
+    writer: PositionedWriter<io::BufWriter<fs::File>>,
     /// a map from key to log pointer which is  represented as a file offset.
     index: BTreeMap<String, u64>,
 }
@@ -17,8 +17,10 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 enum Command {
     Set { key: String, value: String },
-    Rm { key: String },
+    Remove { key: String },
 }
+
+const DATA_FILE_NAME: &str = "datafile";
 
 impl KvStore {
     /// Opens a `KvStore` from the directory at path.
@@ -26,16 +28,24 @@ impl KvStore {
     /// This will create a new directory if the given one does not exist.
     pub fn open<P: AsRef<Path>>(path: P) -> Result<KvStore> {
         fs::create_dir_all(path.as_ref())?;
-        let datafile = path.as_ref().join("datafile");
-        let file = fs::OpenOptions::new()
+
+        let datafile = path.as_ref().join(DATA_FILE_NAME);
+        let mut index = BTreeMap::new();
+        let mut file = fs::OpenOptions::new()
             .create(true)
-            .append(true)
-            .read(true)
+            .write(true)
             .open(&datafile)?;
+        let offset = file.seek(io::SeekFrom::End(0))?;
+
+        build_index(&datafile, &mut index)?;
+
         Ok(KvStore {
             datafile,
-            w: io::BufWriter::new(file),
-            index: BTreeMap::new(),
+            writer: PositionedWriter {
+                w: io::BufWriter::new(file),
+                offset,
+            },
+            index,
         })
     }
 
@@ -43,14 +53,18 @@ impl KvStore {
     ///
     /// Commands are serialized in JSON format for easier debugging.
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
-        let log = Command::Set { key, value };
-        serde_json::to_writer(&mut self.w, &log)?;
+        let command = Command::Set { key, value };
+        let offset = self.writer.offset;
+        serde_json::to_writer(&mut self.writer, &command)?;
+        if let Command::Set { key, .. } = command {
+            self.index.insert(key, offset);
+        }
         Ok(())
     }
 
     /// Get a value by key.
     pub fn get(&mut self, key: String) -> Result<Option<String>> {
-        self.build_index()?;
+        self.writer.flush()?;
         let op = self.index.get(&key);
         let p = match op {
             Some(p) => p.clone(),
@@ -79,35 +93,54 @@ impl KvStore {
 
     /// Remove a value by key.
     pub fn remove(&mut self, key: String) -> Result<()> {
-        self.build_index()?;
         match self.index.get(&key) {
             Some(_) => {
-                let log = Command::Rm { key };
-                serde_json::to_writer(&mut self.w, &log)?;
+                let command = Command::Remove { key };
+                serde_json::to_writer(&mut self.writer, &command)?;
+                if let Command::Remove { key } = command {
+                    self.index.remove(&key);
+                }
                 Ok(())
             }
             None => Err(Error::KeyNotFound),
         }
     }
+}
 
-    fn build_index(&mut self) -> Result<()> {
-        self.w.flush()?;
-        let mut file = fs::File::open(&self.datafile)?;
-        let mut stream = serde_json::Deserializer::from_reader(&mut file).into_iter::<Command>();
-        let mut offset = stream.byte_offset() as u64;
-        while let Some(command) = stream.next() {
-            let command = command?;
-            match command {
-                Command::Set { key, value: _ } => {
-                    self.index.insert(key, offset as u64);
-                }
-                Command::Rm { key } => {
-                    self.index.remove(&key);
-                }
+fn build_index(datafile: impl AsRef<Path>, index: &mut BTreeMap<String, u64>) -> Result<()> {
+    let mut file = fs::File::open(datafile)?;
+    let mut stream = serde_json::Deserializer::from_reader(&mut file).into_iter::<Command>();
+    let mut offset = stream.byte_offset() as u64;
+    while let Some(command) = stream.next() {
+        let command = command?;
+        match command {
+            Command::Set { key, .. } => {
+                index.insert(key, offset as u64);
             }
-            offset = stream.byte_offset() as u64;
+            Command::Remove { key } => {
+                index.remove(&key);
+            }
         }
-        Ok(())
+        offset = stream.byte_offset() as u64;
+    }
+    Ok(())
+}
+
+/// PositionedWriter tracks the current writing position as a offset in bytes from the start of the stream.
+struct PositionedWriter<W: Write> {
+    w: W,
+    offset: u64,
+}
+
+impl<W: io::Write> io::Write for PositionedWriter<W> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let n = self.w.write(buf)?;
+        self.offset += n as u64;
+        Ok(n)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.w.flush()
     }
 }
 
